04a3a437dc19a2b8aa382df33728444b365c7857
RaulShahi <rahulfromnepal2013@gmail.com>
2023-01-06 13:16:19 +0545

    added drawer navigation

--- constants/ScreenNames.js
+++ constants/ScreenNames.js
@@ -1,3 +1,5 @@
-export const MEALSCATEGORIES = 'MealsCategories';
+export const MEALSCATEGORIES = 'Categories';
 export const MEALSOVERVIEW = 'MealsOverview';
 export const MEALDETAILS = 'MealDetails';
+export const DRAAWERSCREEN = 'DrawerScreen';
+export const FAVORITES = 'Favorites';
